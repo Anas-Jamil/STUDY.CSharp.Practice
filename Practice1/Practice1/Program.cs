9aff1a0102920b3b4a4769890f015ca7e3620031
Anas Jamil <146596074+Anas-Jamil@users.noreply.github.com>
2025-03-17 01:01:07 -0400

    Updating some good practices

--- Practice1/Practice1/Program.cs
+++ Practice1/Practice1/Program.cs
@@ -7,28 +7,29 @@ namespace Practicing
         static void Main(string[] args)
         {
             float myNum = 5.65F;
-            int plus = 20 + 4;
-            bool ifPrint = false;
+            int random;
 
             Console.WriteLine("Please enter first name: ");
             string fName = Console.ReadLine();
             Console.WriteLine("Please enter last name: ");
             string lName = Console.ReadLine();
 
-            Console.WriteLine("Please enter an integer: ");
-            int random = Convert.ToInt32(Console.ReadLine());
 
-            if (random >= 25)
+            Console.WriteLine("Please enter an integer: ");
+            if (!int.TryParse(Console.ReadLine(), out random))
             {
-                ifPrint = true;
+                Console.WriteLine("Invalid Input!");
+                random = 0;
             }
+
+            bool ifPrint = random >= 25;
 
             Console.WriteLine(fName.Length > lName.Length
                 ? "First name is longer than last"
                 : "Last name is longer than first");
 
             Console.WriteLine($"Full Name is: {fName} {lName}");
-            Console.WriteLine("Result is: " + plus + " " + myNum);
+            Console.WriteLine("Result is: " + myNum);
             Console.WriteLine("Status is: " + ifPrint);
             Console.WriteLine("Max name count is: " + Math.Max(fName.Length, lName.Length));
 
