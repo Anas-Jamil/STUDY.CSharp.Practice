6f19b4c16fbca681598e57bb0b7417268e330ceb
Anas Jamil <146596074+Anas-Jamil@users.noreply.github.com>
2025-03-17 00:29:38 -0400

    Add project files.

---
+++ Practice1/Practice1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Practicing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float myNum = 5.65F;
+            int plus = 20 + 4;
+            bool ifPrint = false;
+
+            Console.WriteLine("Please enter first name: ");
+            string fName = Console.ReadLine();
+            Console.WriteLine("Please enter last name: ");
+            string lName = Console.ReadLine();
+
+            Console.WriteLine("Please enter an integer: ");
+            int random = Convert.ToInt32(Console.ReadLine());
+
+            if (random >= 25)
+            {
+                ifPrint = true;
+            }
+            if (fName.Length > lName.Length)
+            {
+                Console.WriteLine("First name is longer than last");
+            }
+            else
+            {
+                Console.WriteLine("Last name is longer than first");
+            }
+
+            Console.WriteLine($"Full Name is: {fName} {lName}");
+            Console.WriteLine("Result is: " + plus + " " + myNum);
+            Console.WriteLine("Status is: " + ifPrint);
+            Console.WriteLine("Max name count is: " + Math.Max(fName.Length, lName.Length));
+
+        }
+
+    }
+}
